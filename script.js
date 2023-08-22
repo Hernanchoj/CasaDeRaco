beb12a656891095e55bb4fd72c1086d5709d4cb6
Hernancho <hernancolombres123@gmail.com>
2023-08-22 11:45:55 -0300

    version con carrusel

--- script.js
+++ script.js
@@ -1,14 +1,41 @@
-const panels = document.querySelectorAll('.panel')
+const imgs = document.getElementById('imgs');
+const leftBtn = document.getElementById('left');
+const rightBtn = document.getElementById('right');
 
-panels.forEach(panel => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
-})
+const img = document.querySelectorAll('#imgs img');
 
-function removeActiveClasses() {
-    panels.forEach(panel => {
-        panel.classList.remove('active')
-    })
+let idx = 0;
+
+let interval = setInterval(run, 2000);
+
+function run() {
+    idx++;
+    changeImage();
 }
+
+function changeImage() {
+    if (idx > img.length - 1) {
+        idx = 0;
+    } else if (idx < 0) {
+        idx = img.length - 1;
+    }
+
+    imgs.style.transform = `translateX(${-idx * 100}%)`;
+}
+
+function resetInterval() {
+    clearInterval(interval);
+    interval = setInterval(run, 2000);
+}
+
+rightBtn.addEventListener('click', () => {
+    idx++;
+    changeImage();
+    resetInterval();
+});
+
+leftBtn.addEventListener('click', () => {
+    idx--;
+    changeImage();
+    resetInterval();
+});
